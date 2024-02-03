7d3eec1651c5e4efdfa99b513e8e26c6f3882545
Mehdialaoui02 <mehdi2002alaoui@gmail.com>
2024-02-03 19:44:22 +0100

    Add exception for non existant exercises in the database

---
+++ fitapp/src/main/java/com/fitapp/fitapp/exception/ExercisetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.fitapp.fitapp.exception;
+
+public class ExercisetNotFoundException extends RuntimeException{
+    public ExercisetNotFoundException(String message) {
+        super(message);
+    }
+}
